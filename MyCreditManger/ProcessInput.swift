b62a3a3380a67bcfbab884c104c9810b81ef6347
window1 <doserack@gmail.com>
2022-11-30 05:41:30 +0900

    Refactor

--- MyCreditManger/ProcessInput.swift
+++ MyCreditManger/ProcessInput.swift
@@ -8,60 +8,11 @@
 import Foundation
 
 class ProcessInput {
-    var menuValue: String?
-    var inputValue: String?
     let gradeToPoint:[String : Double] = ["A+" : 4.5, "A" : 4.0,
                                          "B+" : 3.5, "B" : 3.0,
                                          "C+" : 2.5, "C" : 2.0,
                                          "D+" : 1.5, "D" : 1,
                                          "F" : 0]
-    //MARK: - 입력정보 분기
-    func start() -> String {
-        print("""
-              원하는 기능을 입력해 주세요
-              1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X:종료
-              """)
-        menuValue = readLine()
-        switch menuValue?.uppercased() {
-        case MenuType.addStudent.rawValue:
-            print("추가할 학생의 이름을 입력해주세요")
-            inputValue = readLine()
-            addStudent(inputValue ?? "")
-            break
-        case MenuType.deleteStudent.rawValue:
-            print("삭제할 학생의 이름을 입력해주세요")
-            inputValue = readLine()
-            deleteStudent(inputValue ?? "")
-            break
-        case MenuType.addGrade.rawValue:
-            print("""
-                  성적을 추가할 학생의 이름, 과목 이름, 성적(A+, A, F 등)을 띄어쓰기로 구분하여 차례로 작성해 주세요"
-                  입력예) Mickey Swift A+"
-                  만약 학생의 성적 중 해당 과목이 존재하면 기존 점수가 갱신됩니다.
-                  """)
-            inputValue = readLine()
-            addGrade(inputValue ?? "")
-            break
-        case MenuType.deleteGrade.rawValue:
-            print("""
-                  성적을 삭제할 학생의 이름, 과목 이름 띄어쓰기로 구분하여 차례로 작성해주세요.
-                  입력예) Mickey Swift
-                  """)
-            inputValue = readLine()
-            deleteGrade(inputValue ?? "")
-            break
-        case MenuType.viewAverage.rawValue:
-            print("평점을 알고 싶은 학생의 이름을 입력해 주세요")
-            inputValue = readLine()
-            viewGrade(inputValue ?? "")
-            break
-        case MenuType.exit.rawValue:
-            print("프로그램을 종료합니다.")
-        default:
-            print("뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해 주세요")
-        }
-        return menuValue!.uppercased()
-    }
 
     //MARK: - 입력정보 처리
     func addStudent(_ name: String?) {
--- MyCreditManger/main.swift
+++ MyCreditManger/main.swift
@@ -18,11 +18,61 @@ enum MenuType: String {
 //MARK: - Start
 var stInfo = Student()
 var stList = StudentList(students: [stInfo])
+var menuValue: String?
+var inputValue: String?
 var value = ""
 let process = ProcessInput()
 
 while(value != MenuType.exit.rawValue) {
-   value = process.start()
+   value = start()
+}
+
+//MARK: - 입력정보 분기
+func start() -> String {
+    print("""
+          원하는 기능을 입력해 주세요
+          1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X:종료
+          """)
+    menuValue = readLine()
+    switch menuValue?.uppercased() {
+    case MenuType.addStudent.rawValue:
+        print("추가할 학생의 이름을 입력해주세요")
+        inputValue = readLine()
+        process.addStudent(inputValue ?? "")
+        break
+    case MenuType.deleteStudent.rawValue:
+        print("삭제할 학생의 이름을 입력해주세요")
+        inputValue = readLine()
+        process.deleteStudent(inputValue ?? "")
+        break
+    case MenuType.addGrade.rawValue:
+        print("""
+              성적을 추가할 학생의 이름, 과목 이름, 성적(A+, A, F 등)을 띄어쓰기로 구분하여 차례로 작성해 주세요"
+              입력예) Mickey Swift A+"
+              만약 학생의 성적 중 해당 과목이 존재하면 기존 점수가 갱신됩니다.
+              """)
+        inputValue = readLine()
+        process.addGrade(inputValue ?? "")
+        break
+    case MenuType.deleteGrade.rawValue:
+        print("""
+              성적을 삭제할 학생의 이름, 과목 이름 띄어쓰기로 구분하여 차례로 작성해주세요.
+              입력예) Mickey Swift
+              """)
+        inputValue = readLine()
+        process.deleteGrade(inputValue ?? "")
+        break
+    case MenuType.viewAverage.rawValue:
+        print("평점을 알고 싶은 학생의 이름을 입력해 주세요")
+        inputValue = readLine()
+        process.viewGrade(inputValue ?? "")
+        break
+    case MenuType.exit.rawValue:
+        print("프로그램을 종료합니다.")
+    default:
+        print("뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해 주세요")
+    }
+    return menuValue!.uppercased()
 }
 
 
